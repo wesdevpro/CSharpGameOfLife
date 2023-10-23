eeb4951fd7113a69769b7ec87f8da38e0cb684d8
Wesley Ford <wes@wesdev.pro>
2023-10-23 13:01:26 -0500

    Initialized `Project` Structure
    1. Created a `.csproj` library for modeling `The Game of Life`
    2. Created a `.csproj` app for running and presenting `The Game of Life`

---
+++ src/GameOfLifeLibrary/GameOfLife.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace GameOfLifeLibrary
+{
+    public class GameOfLife
+    {
+        public const int cellWidth = 0;
+        public const int cellHeight = 0;
+
+        private int width,
+                    height;
+        private List<Cell> cells = new List<Cell>(0);
+
+        GameOfLife() { }
+
+        public void Start() { 
+            Draw();
+            Update();
+        }
+
+        public void End() { }
+
+        public void Update() { }
+        public void Draw() { }
+    }
+}
